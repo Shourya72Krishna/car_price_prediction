6db4ed53b6e121c41e7c5a6b722bdb3a39032546
Shourya Garg <shouryakrishna21052007@gmail.com>
2026-01-10 14:45:38 +0530

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, BigInteger
 from sqlalchemy.sql import func
 from app.database import Base
 
